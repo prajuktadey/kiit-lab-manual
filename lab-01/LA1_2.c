3ce91c982b0b7177c6b10fd61d60a9cafd312c9a
Prajukta Dey <theprajuktadey@gmail.com>
2021-09-29 17:59:48 +0530

    Added a file.

---
+++ lab-01/LA1_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+//WAP to display “KIMS” using the character ‘#’.//
+
+int main () 
+{
+ printf("#    # ### #     #  #####\n");;  
+ printf("#   #   #  ##   ##  #     #\n"); 
+ printf("#  #    #  # # # #  #\n");       
+ printf("###     #  #  #  #  #####\n");  
+ printf("#  #    #  #     #        #\n"); 
+ printf("#   #   #  #     # #      #\n"); 
+ printf("#    # ### #     #  #####\n");  
+                            
+   return (0);
+}
