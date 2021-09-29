75f4a27cfb617678646dd310d8efea56a3bfdfd3
Prajukta Dey <90215092+prajuktadey@users.noreply.github.com>
2021-09-29 10:43:58 +0530

    Create SA2_1.c

---
+++ lab-02/SA2_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+//WAP to perform the addition of two integers and display the result.//
+
+int main ()
+{
+ int a;
+ int b;
+
+ printf("Input an integer:");
+ scanf("%d", &a);
+
+ printf("Input another integer:");
+ scanf("%d", &b);
+
+
+ printf("%d+ %d= %d\n",a,b,a+b);
+ return 0;
+}
