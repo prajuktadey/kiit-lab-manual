a1cde0b1c5e80b42816acaf5b92ae80e7a3742b3
Prajukta Dey <theprajuktadey@gmail.com>
2021-09-29 18:13:10 +0530

    Added a file.

---
+++ lab-01/HA1_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*WAP to print your BIO-DATA (Name, Regd.no”, Branch, JEE Rank, Gender, Phone no.,
+Address etc.) using printf statement.*/
+
+int main () 
+{
+  printf("My BIO DATA: \n");
+  printf("Regd.No: STU1234 \n");
+  printf("Branch: CSE \n");
+  printf("JEE RANK: 4,40,000\n");
+  printf("Gender: Female \n");
+  printf("Address: 123, Princeton Road\n");
+
+
+   return (0);
+   
+}
