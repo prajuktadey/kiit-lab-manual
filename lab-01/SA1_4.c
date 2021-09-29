ee93e0e7eb437058826fc0ffe7c4af899747dd1f
Prajukta Dey <theprajuktadey@gmail.com>
2021-09-29 14:43:30 +0530

    Added a file.

---
+++ lab-01/SA1_4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//WAP to display “IIT” using the character ‘*’.//
+
+int main()
+{
+printf("\n\n");
+printf("***** ***** ***** \n");
+printf("  *     *     * \n");
+printf("  *     *     * \n");
+printf("  *     *     * \n");
+printf("  *     *     * \n");
+printf("  *     *     * \n");
+printf("***** *****   * \n");
+
+return 0;
+}
