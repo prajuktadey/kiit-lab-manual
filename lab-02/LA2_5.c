09435c80a52c8225d5eb4aa3d2c2b2432a2a925e
Prajukta Dey <theprajuktadey@gmail.com>
2021-09-29 21:31:54 +0530

    Added a file.

---
+++ lab-02/LA2_5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+//WAP to swap two integer numbers without using third variable.//
+
+int main () 
+{
+  int a;
+  int b;
+
+  printf("Enter an integer number: ");
+  scanf("%d", &a);
+
+  printf("Enter an integer number: ");
+  scanf("%d", &b);
+
+  a = a + b; 
+  b = a - b; 
+  a = a - b; 
+
+  printf("The swapped values for a and b: %d and %d ", a,b);
+  
+  return (0);
+}
