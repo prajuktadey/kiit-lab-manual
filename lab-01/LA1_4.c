53e11e1f3cff934c20de16403a8b13d0b61dfe25
Prajukta Dey <theprajuktadey@gmail.com>
2021-09-29 18:08:05 +0530

    Added a file.

---
+++ lab-01/LA1_4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//WAP to display the message of LA 1.3 by using single printf statement.//
+
+int main () 
+{ 
+  printf("If The End Is Good,\nThen It Is Good,\nWhatever Be The Means.");
+  return (0);
+
+}
