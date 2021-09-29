3e8c103bd1e4a2ca8e084bf2f566f5706ad538fc
Prajukta Dey <theprajuktadey@gmail.com>
2021-09-29 18:03:22 +0530

    Added a file.

---
+++ lab-01/LA1_3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*WAP to display the following message by using multiple printf statement.
+If The End Is Good,
+Then It Is Good,
+Whatever Be The Means.*/
+
+int main () 
+{
+  printf("If The End Is Good,\n");
+  printf("Then It Is Good,\n");
+  printf("Whatever Be The Means.\n");
+
+   return (0);
+}
