618381981ad65ae543fcb2e6865df753ec176bab
Prajukta Dey <theprajuktadey@gmail.com>
2021-09-29 14:48:35 +0530

    Added a file.

---
+++ lab-01/SA1_5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*WAP to display the following message by using multiple printf statement.
+A Good End
+Can Only Be Achieved
+Only By Good Means.*/
+
+int main () 
+{
+   printf("A Good End\n");
+   printf("Can Only Be Achieved\n");
+   printf("Only By Good Means.\n");
+   
+   return 0;
+}
