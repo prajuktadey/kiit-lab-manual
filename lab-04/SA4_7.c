7ec760ab63f9b90d3ecd447fd9dfa323c68ecddc
Prajukta Dey <theprajuktadey@gmail.com>
2021-10-02 18:14:07 +0530

    Added a file.

---
+++ lab-04/SA4_7.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+//WAP to check whether an input integer is perfect number or not.//
+
+int main () 
+{
+int n,i,sum=0;
+
+printf("Enter any number: ");
+scanf("%d",&n);
+
+for(i=1;i<n;i++)
+{
+if(n%i==0)
+{
+sum+=i;
+}
+}
+if(n==sum)
+{
+printf("%d is a perfect number." ,n);
+}
+else
+{
+printf("%d is not a perfect number." ,n);
+}
+
+return 0;
+}
