79d89e68cedaa2afdff3952ac0273e94a691e9b3
Prajukta Dey <theprajuktadey@gmail.com>
2021-10-02 16:33:00 +0530

    Added a file.

---
+++ lab-04/SA4_4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//WAP to calculate the sum of digits of a given number.//
+
+   int main () 
+   {
+   int n,i,sum=0,d,num;
+
+   printf("\nEnter a number: ");
+   scanf("%d",&n);
+   
+   num=n;
+   while(n!=0)
+   {
+   d=n%10;
+   sum=sum+d;
+   n=n/10;
+   }
+   printf("\nSum of digits of the number %d is = %d",num,sum);
+
+   return 0;
+}
