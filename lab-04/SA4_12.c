975dbe21c765a5b43f32f4c60876e8edbcd6350e
Prajukta Dey <theprajuktadey@gmail.com>
2021-10-04 10:24:06 +0530

    Added a file.

---
+++ lab-04/SA4_12.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*WAP to print the following pattern for n rows. 
+Ex. for n=5 rows*/
+
+int main()
+{
+int n,i,j,k;
+
+printf("Enter how many rows: ");
+scanf("%d",&n);
+
+printf("\n");
+
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=n-i;j++)
+printf(" ");
+for(k=1;k<=i;k++)
+printf("* ");
+printf("\n");
+}
+return 0;
+}
