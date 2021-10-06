1e067512a4439d6b30fdf96f1923678f30dd9d48
Prajukta Dey <theprajuktadey@gmail.com>
2021-10-06 10:23:09 +0530

    Added a file.

---
+++ lab-04/LA4_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//WAP to print your name 5 times using while loop.//
+
+int main () 
+{
+   for(int i=0; i<5; i++)
+   {
+       printf("Prajukta Dey\n");
+   }
+   return 0;
+}
