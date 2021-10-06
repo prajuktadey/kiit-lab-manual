e5a78e15fd9559d527111eec97badba2562cd22a
Prajukta Dey <theprajuktadey@gmail.com>
2021-10-06 10:17:43 +0530

    Added a file.

---
+++ lab-04/SA4_14.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+int n,i,j,k;
+
+printf("Enter a number to form a pyramid: ");
+scanf("%d",&n);
+
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=n-i;j++)
+printf(" ");
+for(k=1;k<=i;k++)
+printf("%d",k);
+for(k=i-1;k>0;k--)
+printf("%d",k);
+printf("\n");
+}
+
+return 0;
+}
