f07cf73798c15b9d16961e37c060ab9e8a1b2db7
Prajukta Dey <theprajuktadey@gmail.com>
2021-10-06 10:25:38 +0530

    Added a file.

---
+++ lab-04/LA4_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () 
+{
+   int n;
+
+   printf("Enter number of times: ");
+   scanf("%d", &n);
+
+   for(int i=0;i<n; i++)
+   {
+       printf("Prajukta Dey\n");
+   }
+   return 0;
+}
