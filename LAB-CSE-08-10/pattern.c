3d029c5b6a016a97d5bd88483172488785b2711f
Prajukta Dey <theprajuktadey@gmail.com>
2021-10-08 17:46:27 +0530

    Added a file.

---
+++ LAB-CSE-08-10/pattern.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//WAP to display “KIIT” using the character ‘*’.//
+
+int main()
+{
+printf("\n\n");
+printf(" *    *   *****  *****  ***** \n");
+printf(" *   *      *      *      *  \n");
+printf(" *  *       *      *      * \n");
+printf(" * *        *      *      * \n");
+printf(" *  *       *      *      * \n");
+printf(" *   *      *      *      * \n");
+printf(" *     *  *****  *****    * \n");
+
+return 0;
+}
