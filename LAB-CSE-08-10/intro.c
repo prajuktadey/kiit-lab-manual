040b823090d69f548bf44fd992650299d2385cd8
Prajukta Dey <theprajuktadey@gmail.com>
2021-10-08 17:49:54 +0530

    Added a file.

---
+++ LAB-CSE-08-10/intro.c
@@ -0,0 +1,12 @@
+// To display my name, roll number, branch and section//
+#include <stdio.h>
+
+int main () 
+{
+   printf("Name: Prajukta Dey\n");
+   printf("Roll Number: 21052263\n");
+   printf("Branch: CSE\n");
+   printf("Section: B22\n");
+
+   return 0;
+}
