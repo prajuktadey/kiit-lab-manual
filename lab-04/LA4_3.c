6b99c533a6b7e7640c8b7999d7b41ce13c463959
Prajukta Dey <theprajuktadey@gmail.com>
2021-10-21 17:35:30 +0530

    added files

---
+++ lab-04/LA4_3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//WAP to print the series as 1 2 3 4 5 6 7 ..........100.//
+
+int main () 
+{
+   for(int i=1; i<=100; i++)
+   {
+       printf("%d", i );
+       printf("");
+   }
+   return 0;
+}
