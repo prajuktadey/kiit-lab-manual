9b7943d99af55e0fecbcdbbd428a077322556d6e
Prajukta Dey <theprajuktadey@gmail.com>
2021-11-11 19:11:14 +0530

    Added a file.

---
+++ LAB-CSE-11-11/sum.c
@@ -0,0 +1,17 @@
+//wapt to print the sum of two numbers
+#include <stdio.h>
+
+int main () 
+{
+   int a,b,sum;
+   printf("Enter the number a: ");
+   scanf("%d", &a);
+   printf("/nEnter the number b: ");
+   scanf("%d", &b);
+
+   sum=a+b;
+
+   printf("Sum= %d", sum);
+   return 0;
+
+}
