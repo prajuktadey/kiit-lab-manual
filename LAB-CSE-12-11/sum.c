cf63659992f2011af7830def46868af106d5ef04
Prajukta Dey <theprajuktadey@gmail.com>
2021-11-12 17:22:20 +0530

    Added a file.

---
+++ LAB-CSE-12-11/sum.c
@@ -0,0 +1,21 @@
+//wap to three inputs and print sum of the sides if those are sides of triangle else display 0
+
+#include <stdio.h>
+
+int main () 
+{
+   int a,b, c, t, s;
+
+   printf("Enter side-1: ");
+   scanf("%d", &a);
+   printf("Enter side-2: ");
+   scanf("%d", &b);
+   printf("Enter side-3: ");
+   scanf("%d", &c);
+
+   t=(((a+b)>c) && ((b+c)>a) && ((c+a)>b));
+   s=t*(a+b+c);
+
+   printf("\nDo they satisfy triangle property?1 for yes and 0 for no:  %d" ,t);
+   return 0;
+}
