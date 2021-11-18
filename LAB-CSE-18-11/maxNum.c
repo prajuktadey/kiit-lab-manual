b3d80a644e214d90229acd719ff8dbe2e58116d5
Prajukta Dey <theprajuktadey@gmail.com>
2021-11-18 19:37:29 +0530

    Added a file.

---
+++ LAB-CSE-18-11/maxNum.c
@@ -0,0 +1,24 @@
+//wap to print the largest among three numbers(a,b,c)
+#include <stdio.h>
+
+int main () 
+{
+   int a, b,c, max;
+   printf("\nEnter 3 numbers: ");
+   scanf("%d %d %d", &a, &b, &c);
+   max=a;
+
+   if(b>max)
+   {
+       max=b;
+   }
+
+   if(c>max)
+   {
+       max=c;
+   }
+   
+
+   printf("\nThe largest number is: %d", max);
+   return 0;
+}
