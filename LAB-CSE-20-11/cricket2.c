481f3d8d0eb598d0f40cbdbefa71e0dbf72ec787
Prajukta Dey <theprajuktadey@gmail.com>
2021-11-18 20:04:42 +0530

    Added a file.

---
+++ LAB-CSE-20-11/cricket2.c
@@ -0,0 +1,19 @@
+//enter score of 10 cricket players and count number of century scored. Discard the -ve score
+#include <stdio.h>
+
+int main () 
+{
+   int score, i=0, count=0;
+   again:
+   printf("\nEnter the score of next player number %d: ", i);
+   scanf("%d", &score);
+   if(score<0) goto again;
+
+   count+=score/100;
+   i++;
+   if(i<10) goto again;
+
+   printf("Total number of players who made a century: %d", count);
+   
+   return 0;
+}
