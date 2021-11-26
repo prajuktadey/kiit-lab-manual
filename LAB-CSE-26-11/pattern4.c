612f79ba9ace4c6210221db6ad766c2746a5f60d
Prajukta Dey <theprajuktadey@gmail.com>
2021-11-26 18:35:07 +0530

    Added a file.

---
+++ LAB-CSE-26-11/pattern4.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+  int i,j,n;   // n is the number of rows//
+
+  printf("Enter the number of rows to be printed: ");
+  scanf("%d",&n);
+
+  for(i=0;i<n;i++)
+  {
+     for(j=0;j<(n-1-i);j++)
+        printf(" ");
+     for(j=0;j<(2*i+1);j++)
+        printf("$");
+     for(j=0;j<(n-1-i);j++)
+        printf(" ");
+    for(j=0;j<(n-1-i);j++)
+        printf(" ");
+    for(j=0;j<(2*i+1);j++)
+        printf("$");
+
+    printf("\n");
+}
+
+return 0;
+}
