be145809d8c5da366df2e1038d5d86836e2309a5
Prajukta Dey <theprajuktadey@gmail.com>
2021-12-03 19:04:31 +0530

    Added a file.

---
+++ LAB-CSE-3-12/display2.c
@@ -0,0 +1,21 @@
+//display
+#include<stdio.h>
+int display2( int n)
+{
+  int i;
+  char a;
+
+  printf("ENTER VARIABLE TO BE PRINTED");
+  scanf("%c",&a);
+  
+  for(i=0;i<n;i++)
+  printf("%c",a);
+
+  printf("\n");
+  return 0;
+}
+
+void main()
+{
+  display2(30);
+}
