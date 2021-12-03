7253bf4609848d204da5f89745f0e78c47de0bf7
Prajukta Dey <theprajuktadey@gmail.com>
2021-12-03 15:42:47 +0530

    Added a file.

---
+++ LAB-CSE-2-12/power.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,power,i ;
+    power = 1;
+
+    printf("Enter value of a: " );
+    scanf("%d",&a);
+
+    printf("Enter value of b: ");
+    scanf("%d",&b);
+
+    for ( i = 1 ; i <= b ; i ++ )
+    {
+        power = power*a;
+    }
+
+    printf("%d^%d is %d\n",a,b,power);
+    return 0;
+}
