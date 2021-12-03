d3af2a69844b1d09b51b24d06cd30a2d9db0aa7d
Prajukta Dey <theprajuktadey@gmail.com>
2021-12-03 19:05:04 +0530

    Added a file.

---
+++ LAB-CSE-3-12/swap.c
@@ -0,0 +1,28 @@
+//swap two numbers 
+#include<stdio.h>
+
+void swap(int, int);  
+  
+int main()  
+{ 
+    int a, b; 
+    printf("Enter values for a: ");
+    scanf("%d", &a); 
+    printf("Enter values for b: ");
+    scanf("%d", &b); 
+    printf("Before swapping: a: %d and b: %d\n", a, b);  
+  
+    swap(a, b);  
+    return 0;  
+}  
+  
+void swap(int x, int y)  
+{  
+    int temp;  
+  
+    temp=x;  
+    x=y;  
+    y=temp;  
+  
+    printf("After swapping: a: %d and b: %d\n", x, y);  
+}  
