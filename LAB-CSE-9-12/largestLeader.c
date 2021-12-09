9d224820fda6c0adb63cc9cb5d3d8bd32a012c42
Prajukta Dey <theprajuktadey@gmail.com>
2021-12-09 17:54:54 +0530

    Added a file.

---
+++ LAB-CSE-9-12/largestLeader.c
@@ -0,0 +1,51 @@
+//function to find the largest number between n numbers and another function to count the number of leaders//
+#include <stdio.h>
+   int findmax(int n)
+   {
+      int nextnum, largest=0, i;
+      for(i=0; i<n;i++)
+      {
+         printf("\nEnter the next number: ");
+         scanf("%d", &nextnum);
+         if(nextnum > largest)
+             largest=nextnum;
+      }
+      return largest;
+   }
+
+   int countLeader(int n)
+   {
+      int nextnum, leader=0, i, count=0;
+      for(i=0;i<n;i++)
+      {
+         printf("\nEnter next number: ");
+         scanf("%d", &nextnum);
+         if(nextnum>leader)
+         {
+            leader=nextnum;
+            count++;
+         }
+      }
+      return count;
+   }
+
+   #include <stdio.h>
+   //function definition 
+   int main()
+   {
+      int a;
+      a=findmax(10);
+      printf("\nMax is %d.", a);
+      printf("\nMax is %d.", findmax(15));
+
+      a= countleader(20);
+      printf("\nNo. of Leaders are %d", a);
+      printf("\nNo. of Leaders are %d", countleader(10));
+
+      return 0;
+
+   }
+
+
+
+  
