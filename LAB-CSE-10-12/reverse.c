f12a78f7b2cc9c5de40eb6f8d43a4767b73d9fa2
Prajukta Dey <theprajuktadey@gmail.com>
2021-12-10 17:53:39 +0530

    Modified.

--- LAB-CSE-10-12/reverse.c
+++ LAB-CSE-10-12/reverse.c
@@ -7,10 +7,11 @@ void main()
     for(i=0;i<10;i++)
     {
         scanf("%d",&a[i]);
+        
     }
-    
+
     for(i=9;i>=0;i--)
     {
-        printf("%d\t",a[i]);
+        printf("%d ",a[i]);
     }
 }
