fe1ea54e5f585bb67c8d3cde3da8542ac9a002c7
Prajukta Dey <theprajuktadey@gmail.com>
2021-12-10 17:49:59 +0530

    Modified.

--- LAB-CSE-10-12/max.c
+++ LAB-CSE-10-12/max.c
@@ -3,9 +3,10 @@
 int main()
 {
     int a[5],i,max;
-    printf("Enter 5 numbers: \n");
+    
     for(i=0;i<5;i++)
     {
+        printf("Enter number %d: ", i+1);
         scanf("%d",&a[i]);
     }
     max=a[0];
