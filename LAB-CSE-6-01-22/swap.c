6c91e4d2cec1161216628dcf3a73ded89d891b62
Prajukta Dey <theprajuktadey@gmail.com>
2022-01-06 17:55:40 +0530

    Added a file.

---
+++ LAB-CSE-6-01-22/swap.c
@@ -0,0 +1,22 @@
+//swap
+#include <stdio.h>
+ 
+int main()
+{
+   int x, y, *a, *b, temp;
+ 
+   printf("Enter the value of x and y: ");
+   scanf("%d%d", &x, &y);
+ 
+   a = &x;
+   b = &y;
+ 
+   temp = *b;
+   *b = *a;
+   *a = temp;
+ 
+   printf("After Swapping\nx = %d\ny = %d\n", x, y);
+ 
+   return 0;
+}
+ 
