e663b80c62d646c6075053a64827ca38b50b227f
Prajukta Dey <theprajuktadey@gmail.com>
2022-01-08 10:32:09 +0530

    Added a file.

---
+++ LAB-CSE-7-01-22/count.c
@@ -0,0 +1,22 @@
+//count number of a in the array elements
+#include<stdio.h>
+int main()
+{
+        
+    char a[] = "Yudhistir", b[] = "Bhim", c[]= "Arjun", d[] = "Nakul", e[] = "Sahadev";
+    char *p[5];
+
+    int i,j,count = 0;
+    p[0]=a; p[1]=b;p[2]=c; p[3] = d ; p[4] = e;
+
+    for(i=0;i<5;i++){
+        for(j=0;p[i][j]; j++)
+        {
+            count += p[i][j]=='a';
+        }
+    }
+
+    printf("\nCount = %d",count);
+    
+    return 0;
+}
