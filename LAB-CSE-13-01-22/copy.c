c57c8083e8bf6bddc41800ea5dea8ebd66ae484d
Prajukta Dey <theprajuktadey@gmail.com>
2022-01-14 10:34:34 +0530

    Added a file.

---
+++ LAB-CSE-13-01-22/copy.c
@@ -0,0 +1,17 @@
+//copying a string 
+#include <stdio.h>
+
+int main () 
+{
+  char i=0, src[100], dst[100];
+
+  printf("Enter String: ");
+  scanf("%s", src);
+
+  while(src[i]!='\0')
+    dst[i]=src[i++];
+
+  dst[i]='\0';
+  printf("\nDestination String: %s", dst);
+   return 0;
+}
